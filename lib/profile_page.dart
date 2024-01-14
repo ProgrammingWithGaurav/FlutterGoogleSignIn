5f2359aef99f1b90fb51c9563fc1efc97eea1b27
ProgrammingWithGaurav <gaurav299kumar@gmail.com>
2024-01-14 16:02:03 +0530

    Done Profile Page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,9 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilPage extends StatelessWidget {
-  const ProfilPage({super.key});
+  // create a user class
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
   Future<void> logout() async {
     final GoogleSignIn googleSignIn = GoogleSignIn();
     await googleSignIn.signOut();
@@ -12,7 +14,12 @@ class ProfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('User Details')),
+        appBar: AppBar(
+          title: const Text('User Details'),
+          backgroundColor: Colors.lightBlue,
+          centerTitle: true,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        ),
         body: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
@@ -23,6 +30,32 @@ class ProfilPage extends StatelessWidget {
                 Navigator.pop(context);
               },
               child: const Text('Logout')),
+          Text(
+            'User Name: ${_auth.currentUser?.displayName}',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'User Email: ${_auth.currentUser?.email}',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // Image
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(75),
+                child: Image.network(
+                  _auth.currentUser?.photoURL ?? '',
+                ),
+              ),
+            ),
+          ),
         ])));
   }
 }
